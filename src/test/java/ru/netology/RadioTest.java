1add7aafe2c0869a3e41c93fdb0a9b88bd962f06
AnastasiyaPilushina <pilushina22@gmail.com>
2021-02-14 14:55:11 +0300

    Initial commit

---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,105 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+   @Test
+   public void increaseChannel() {
+      Radio radio = new Radio();
+      radio.setCurrentChannel(7);
+      radio.increaseChannel();
+      assertEquals(8, radio.getCurrentChannel());
+   }
+
+   @Test
+   void decreaseChannel() {
+      Radio radio = new Radio();
+      radio.setCurrentChannel(8);
+      radio.decreaseChannel();
+      assertEquals(7, radio.getCurrentChannel());
+   }
+
+   @Test
+   void decreaseVolume() {
+      Radio radio = new Radio();
+      radio.setCurrentVolume(6);
+      radio.decreaseVolume();
+      assertEquals(5, radio.getCurrentVolume());
+   }
+
+   @Test
+   void increaseVolume() {
+      Radio radio = new Radio();
+      radio.setCurrentVolume(5);
+      radio.increaseVolume();
+      assertEquals(6, radio.getCurrentVolume());
+   }
+
+   @Test
+   public void increaseVolumeFromMaxVolume() {
+      Radio radio = new Radio();
+      radio.setCurrentVolume(10);
+      radio.increaseVolume();
+      assertEquals(10, radio.getCurrentVolume());
+   }
+
+   @Test
+   public void decreaseVolumeFromMinVolume() {
+      Radio radio = new Radio();
+      radio.setCurrentVolume(0);
+      radio.decreaseVolume();
+      assertEquals(0, radio.getCurrentVolume());
+   }
+
+   @Test
+   public void previousChannelFromMin() {
+      Radio radio = new Radio();
+      radio.setCurrentChannel(0);
+      radio.decreaseChannel();
+      assertEquals(9, radio.getCurrentChannel());
+   }
+
+   @Test
+   public void increaseChannelFromMax() {
+      Radio radio = new Radio();
+      radio.setCurrentChannel(9);
+      radio.increaseChannel();
+      assertEquals(0, radio.getCurrentChannel());
+   }
+
+   @Test
+   public void increaseVolumeFromValue() {
+      Radio radio = new Radio();
+      radio.setCurrentVolume(25);
+      radio.increaseVolume();
+      assertEquals(10, radio.getCurrentVolume());
+   }
+
+   @Test
+   public void decreaseVolumeFromValue() {
+      Radio radio = new Radio();
+      radio.setCurrentVolume(25);
+      radio.decreaseVolume();
+      assertEquals(9, radio.getCurrentVolume());
+   }
+
+   @Test
+   public void increaseChannelFromValue() {
+      Radio radio = new Radio();
+      radio.setCurrentChannel(20);
+      radio.increaseChannel();
+      assertEquals(0, radio.getCurrentChannel());
+   }
+
+   @Test
+   public void previousChannelFromValue() {
+      Radio radio = new Radio();
+      radio.setCurrentChannel(30);
+      radio.decreaseChannel();
+      assertEquals(8, radio.getCurrentChannel());
+   }
+
+   }
